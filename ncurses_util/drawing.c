dd43ba32b0648a6eb9857b779470c606f69c1ec3
Benjamin Crawford <bc18888@essex.ac.uk>
2020-07-10 17:33:40 +0100

    Add check for XSI compliance for ONLCR flag.

--- ncurses_util/drawing.c
+++ ncurses_util/drawing.c
@@ -10,6 +10,7 @@ int __init_ncurses() {
 	raw();     // Pass control characters directly to application
 	noecho();  // Don't relay typed characters
 
+#ifdef XSI_SUPPORTED
 	// Use termios to re-enable automatic carriage returns
 	// This is an alternative to using -DONLCR when compiling ncurses
 	struct termios settings;
@@ -17,7 +18,7 @@ int __init_ncurses() {
 	tcgetattr(0, &settings);
 	settings.c_oflag |= ONLCR;
 	tcsetattr(0, TCSADRAIN, &settings);
-
+#endif /* XSI_SUPPORTED */
 	return 0;  // Always succeed
 }
 
--- ncurses_util/drawing.h
+++ ncurses_util/drawing.h
@@ -1,6 +1,14 @@
 #ifndef GT_NCURSES_UTIL_DRAWING
 #define GT_NCURSES_UTIL_DRAWING
 
+//We require XSI compliance for the ONLCR terminal flag
+#if !defined(_WIN32) && (defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__)))
+#include <unistd.h>
+#if (_XOPEN_UNIX > -1)
+#define XSI_SUPPORTED
+#endif /* _XOPEN_UNIX */
+#endif /* unix */
+
 // Initialise ncurses for the purposes of graph_traversal
 int __init_ncurses();
 int __end_ncurses();
